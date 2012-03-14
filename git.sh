408d6e923457efda36a150274ea739c52ce11edd
Spenser Jones <spenser.e.jones@gmail.com>
2012-03-14 15:13:27 -0600

    Check if git is already installed and up to date before proceeding

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@
 #
 # Instructions:
 #
-# 1. bash -s stable < <(curl -s https://raw.github.com/SpenserJ/42Bash/master/git.sh)
+# 1. bash < <(curl -s https://raw.github.com/SpenserJ/42Bash/master/git.sh)
 # 2. Rejoice
 
 ###############################
@@ -18,9 +18,10 @@ set -e
 ### What /tmp directory do we want to use? ###
 ##############################################
 
-if [ "$1" != "" ];
-  then TMP=${1%/}/git
-  else TMP=/tmp/42Bash-Git
+if [ "$1" != "" ]; then
+  TMP=${1%/}/git
+else
+  TMP=/tmp/42Bash-Git
 fi
 
 #########################################################
@@ -31,12 +32,29 @@ rm -rf $TMP
 mkdir $TMP
 cd $TMP
 
-#################################################
-### Download the latest stable release of Git ###
-#################################################
+################################################
+### Look up the latest stable release of Git ###
+################################################
 
 echo "Scraping Git's website for the latest stable release number"
 GIT_VERSION=$(curl -silent http://git-scm.com/ | sed -n '/id="ver"/ s/.*v\([0-9].*\)<.*/\1/p')
+
+#########################################################################
+### Check if git is already installed, and if so, if it is up to date ###
+#########################################################################
+
+if [ `command -v git` ]; then
+  GIT_INSTALLED=`git --version`
+  if [ ${GIT_INSTALLED#git version } = $GIT_VERSION ]; then
+    echo "Git is already installed ($GIT_INSTALLED) and up to date ($GIT_VERSION)."
+    exit 0
+  fi
+  echo "You currently have Git $GIT_INSTALLED installed, and $GIT_VERSION is the newest. Updating now."
+fi
+
+#################################################
+### Download the latest stable release of Git ###
+#################################################
 
 echo "Downloading & unpacking Git $GIT_VERSION"
 curl http://git-core.googlecode.com/files/git-$GIT_VERSION.tar.gz | tar -xz
