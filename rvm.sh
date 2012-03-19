8702b513fee58d8f7918d366e049027bfd4b8ccc
Spenser Jones <spenser.e.jones@gmail.com>
2012-03-19 10:22:11 -0600

    Added quite a bit of functionality and fixes to RVM

--- rvm.sh
+++ rvm.sh
@@ -28,16 +28,58 @@ while true; do
 set_tmp_directory 'rvm'
 install_pre $APPTMP
 
-######################################################
-### Install RVM, Ruby 1.9.3, and set it as default ###
-######################################################
+#########################################
+### Install prerequisites via apt-get ###
+#########################################
+
+install_dependencies $BUILD_DEPENDENCIES
+
+###################
+### Install RVM ###
+###################
 
 sudo bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 sudo usermod -a -G rvm spenser
 source /etc/profile.d/rvm.sh
-rvm install 1.9.3
+
+###########################################
+### Confirm that RVM installed properly ###
+###########################################
+
+RVM_INSTALLED=`rvm version`
+if [[ "$RVM_INSTALLED" =~ rvm\ ([0-9.]+) ]]; then
+  echo "RVM has installed correctly."
+else
+  echo "RVM failed to install properly."
+  exit 1
+fi
+
+#################################################
+### Install Ruby 1.9.3, and set it as default ###
+#################################################
+
+rvmsudo rvm install 1.9.3
 rvm --default use 1.9.3
 rvm tools rvm-env ruby bash
+
+############################################
+### Confirm that Ruby installed properly ###
+############################################
+
+RUBY_INSTALLED=`ruby -v`
+if [[ "$RUBY_INSTALLED" =~ ruby\ ([0-9.]+) ]]; then
+  if [ ${BASH_REMATCH[1]} = "1.9.3" ]; then
+    echo "Ruby has installed correctly."
+  else
+    echo "Ruby seems to have installed, but you appear to have an old version overriding it. Please remove the following file, and then run 'ruby -v' to confirm that it is now 1.9.3"
+    which ruby
+    exit 1
+  fi
+  
+else
+  echo "Ruby failed to install properly."
+  exit 1
+fi
 
 ############################
 ### Clean up and go home ###
